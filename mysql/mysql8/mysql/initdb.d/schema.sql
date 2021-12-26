3b3dc3fe7e9df61907675db7a5a1ff8d2757dd4c
mutao-net <sasaki.h1026@gmail.com>
2021-12-26 23:09:20 +0900

    update dijango

--- mysql/mysql8/mysql/initdb.d/schema.sql
+++ mysql/mysql8/mysql/initdb.d/schema.sql
@@ -4,3 +4,12 @@ create table users(
   , register_date TIMESTAMP NOT NULL default current_timestamp
   , update_time TIMESTAMP NOT NULL default current_timestamp on update current_timestamp
 ) ENGINE = InnoDB DEFAULT CHARSET = utf8;
+
+
+create table timeline( 
+  id integer PRIMARY KEY 
+  , name varchar (255) NOT NULL
+  , tweet varchar (255) NOT NULL
+  , favorite_count intger default 0
+  , register_date TIMESTAMP NOT NULL default current_timestamp
+) ENGINE = InnoDB DEFAULT CHARSET = utf8;
